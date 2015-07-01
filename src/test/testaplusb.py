41910d3bb439bef731a425c1d5abee2159c0f390
ctyi <1997cui@sina.com>
2015-07-01 16:59:10 +0800

    测试中文

--- src/test/testaplusb.py
+++ src/test/testaplusb.py
@@ -1,7 +1,11 @@
+# coding=utf-8
 from django.test import TestCase
 import simplejson
 
 class TestAplusB(TestCase):
+    """This is a Unit test for aplusb.
+
+    这是一个测试a+b的单元测试。"""
     def test_aplusb(self):
         response = self.client.get('/aplusb/1/2/')
         self.assertEqual(response.status_code, 200, "Server doesn't return 200")
