f28029ac7a3627e6b3d5261250ef811b5b7a266a
zh19970205 <zh970205@mail.ustc.edu.cn>
2015-08-09 22:17:28 +0800

    build and_1 and not_1

--- src/src/biosys/devices_system.py
+++ src/src/biosys/devices_system.py
@@ -1,91 +1,46 @@
 #!/usr/bin/env python
 
 from parts_system import parts_system
-import numpy
+from database import check,find
 
-class substance(object):
-    '''
-    Parameter:
-        string:the name of the substance
-        list:constance of the RNA_create,RNA_die,protein_create,protein_die
-        string:the promotor of connect with this substance
-    '''
-    def __init__(self,name,constance,promotor):
-        self.promoter=promotor
-        self.name=name
-        self.DNA=name+':DNA'
-        self.RNA=name+':RNA'
-        self.protein=name
-        self.RNA_create=constance[0]
-        self.RNA_die=consrtance[1]
-        self.protein_create=constance[2]
-        self.protein_die=constance[3]
-        
-    @property
-    def get_species(self):
-        return [self.DNA,self.RNA,self.protein]
-    
-    @property
-    def get_effect(self):
-        return [[[self.DNA,promotor+":Act"],[self.DNA,self.RNA,self.promoter+":Act"],self.RNA_create,[[],[]]],
-                [[self.RNA],[],self.RNA_die,[[],[]]],
-                [[self.RNA],[self.RNA,self.protein],protein_creat,[[],[]]]
-                [[self.protein],[],],self.protein_die,[[],[]]]
+def check(*arg,**kw):
+    return True
+def find(*arg,**kw):
+    return [[],[]]
 
-class promoter(object):
-    '''
-    Parameter:
-        string:the name of promoter
-        gate:
-            ["Strong"]|
-            ["Not",[["",int],..]]|
-            ["Nor",[["",int]..]]|
-            [Nand[..]
-            ["And",[...]]|
-            [Or[..]|
-    '''
-    def __init__(self,name,active,gate):
-        self.name=name
-        self.active_name=name+":Act"
-        
-    @property
-    def get_species(self):
-        return [self.name,self.active_name]
-    
-    @property
-    def get_effect(self):
-        if gate[0]=="Strong":
-            return [[self.name],[self.active],active,[[],[]]]
-        if (gate[0]=="Not")or(gate[0]=="Nor"):
-            return [[self.name],[self.active],active,[gate[1],[]]]
-        if gate[0]=="Or":
-            return [[self.name],[self.active],0,[[],gate[1]]]
-        if gate[0]=="And":
-            pass
-        if gate[0]=="Nand":
-            pass
+class and_1(parts_system):
+    def __init__(self,promoter,RBS,coding,terminal,**reaction_data):
+        self.promoter=promoter
+        self.RBS=RBS
+        self.coding=coding
+        self.terminal=terminal
+    def get_
+    species_and_effect(self):
+        self.species=[self.promoter,self,RBS,self.coding,self.terminal]
+        self.effect=[
+            [[self.promoter_1],[self.promoter_1,self.RBS],self.reaction_data[promoter_1_to_RBS],find(find(self.promoter_1))]
+            [[self.RBS],[self.coding,self.RBS],self.reaction_data[RBS_to_protein],find(find(self.RBS))]
+        ]
+        return(self.species,self.effect)
 
-class device_system(parts_system):
-    
-    def set_substance(self,substaces):
-        self.substances=[]
-        for single_substance in substances:
-            self.substances.append(substance(single_substance))
-        for single_substance in self.substances:
-            species+=single_substance.get_species
-            effect+=single_substance.get_effect
-    
-    def set_promotors(self,promotors):
-        self.promotors=[]
-        for single_promotors in promotors:
-            self.promotors,append(promotors(single_promotor))
-        for single_promotor in self.promotors:
-            species+=single_promotors.get_species
-            effect+=single_promoters.geteffect
-    
-    def __init__(self,substances,promotors):
-        self.species=[]
-        self.effect=[]
-        self.set_substance(substances)
-        self.set_promoters(promotors)
-        self.set_species_and_effect(self.species,self.effect)
+class not_1(parts_system):
+    def __init__(self,promoter_1,RBS,lacI,terminal_1,promoter_2,coding,terminal_2,**reaction_data):
+        if check("Positive",lac_I,promoter_2)==False:
+            raise Exception()
+        self.promoter_1=promoter_1
+        self.RBS=RBS
+        self.lacI=lacI
+        self.terminal_1=terminal
+        self.promoter_2=promoter_2
+        self.coding=coding
+        self.terminal=terminal
+        self.reaction_data=reaction_data
+    @property
+    def get_species_and_effect(self):
+        self.species=[self.promoter_1,self.RBS,self.lacI,self.promoter_2,self.coding]
+        self.effect=[
+            [[self.promoter_1],[self.promoter_1,self.RBS],self.reaction_data[promoter_1_to_RBS],find(self.promoter_1)],
+            [[self.RBS],[self.lacI,self.RBS],self.reaction_data[RBS_to_lacI],find(self.RBS)],
+            [[self.promoter_2],[self.promoter_2,self.coding],self.reaction_data[promoter_2_to_coding],[[[self.lacI,reaction_data[lacI_de_promoter_2]]],[]]],
+        ]
+        return(self.species,self.effect)
