f0e86f25a977bed207aa5f63fedadeddaad42d1a
needsay <yuxialantian@vip.qq.com>
2015-08-10 18:38:07 +0800

    .

--- src/home.js
+++ src/home.js
@@ -1,31 +1,76 @@
-﻿BioBLESS.home = new function(){
+﻿BioBLESS.simulator = new function() {
 this.stage = new PIXI.Container();
 this.stage.movable_stage = new PIXI.Container();
 this.stage.movable_stage._scale = 1;
-this.DrawGate = function(icon){
-	    var graphics = new PIXI.Graphics();
-		switch(icon){
-		    //case "or":
-            default:
-			    graphics.lineStyle(3, 0x000000, 1);
-	    		graphics.moveTo(0, 25);
-			    graphics.lineTo(30, 25);
-	    		graphics.moveTo(0, 45);
-			    graphics.lineTo(30, 45);
-	    		graphics.moveTo(120, 35);
-			    graphics.lineTo(150, 35);
-	    		graphics.beginFill(0, 0);
-			    graphics.drawRect(30, 0, 90, 70);
-			    graphics.endFill();
+this.DrawGate = function(device){
+		var element = new PIXI.Container();
+		element.graphics = new PIXI.Graphics();
+		element.title = new PIXI.Text(device.name);
+		element.title.anchor.x = element.title.anchor.y = 0.5;
+		element.title.position.x = 75;
+		element.title.position.y = 35;
+		switch(device.icon){
+			case "XOR":;
+			case "AND":;
+		    case "OR":;
+			case "xor":;
+			case "and":;
+		    case "or":
+			    element.graphics.lineStyle(3, 0x000000, 1);
+	    		element.graphics.moveTo(0, 25);
+			    element.graphics.lineTo(30, 25);
+	    		element.graphics.moveTo(0, 45);
+			    element.graphics.lineTo(30, 45);
+	    		element.graphics.moveTo(120, 35);
+			    element.graphics.lineTo(150, 35);
+	    		element.graphics.beginFill(0, 0);
+			    element.graphics.drawRect(30, 0, 90, 70);
+			    element.graphics.endFill();
 				break;
+			case "NOT":
+			case "not":
+			    element.graphics.lineStyle(3, 0x000000, 1);
+	    		element.graphics.moveTo(0, 35);
+			    element.graphics.lineTo(30, 35);
+	    		element.graphics.moveTo(120, 35);
+			    element.graphics.lineTo(150, 35);
+				element.graphics.moveTo(135, 35);
+				element.graphics.lineTo(120, 20);
+	    		element.graphics.beginFill(0, 0);
+			    element.graphics.drawRect(30, 0, 90, 70);
+			    element.graphics.endFill();
+				break;
+			case "NAND":;
+			case "NOR":;
+			case "XNOR":
+			case "nand":;
+			case "nor":;
+			case "xnor":
+			    element.graphics.lineStyle(3, 0x000000, 1);
+	    		element.graphics.moveTo(0, 25);
+			    element.graphics.lineTo(30, 25);
+	    		element.graphics.moveTo(0, 45);
+			    element.graphics.lineTo(30, 45);
+	    		element.graphics.moveTo(120, 35);
+			    element.graphics.lineTo(150, 35);
+				element.graphics.moveTo(135, 35);
+				element.graphics.lineTo(120, 20);
+	    		element.graphics.beginFill(0, 0);
+			    element.graphics.drawRect(30, 0, 90, 70);
+			    element.graphics.endFill();
+				break;
+			default:
+			    alert("Error - 1001!");
 		}
-		return graphics;
+		element.addChild(element.graphics);
+		element.addChild(element.title);
+		return element;
 	};
 
 this.draw = function(devices){
 	    this.stage.movable_stage._scale = 1;
-        var w = BioBLESS.width;
-        var h = BioBLESS.height;
+        var w = PLUMB.width;
+        var h = PLUMB.height;
 		var that = this;
 		var elements = new Array();
 		var waitForDoubleClick = false;
@@ -38,8 +83,8 @@ this.draw = function(devices){
 				var that = this;
 				var next = function() {
                     if(a.responseJSON){
-                        BioBLESS.device.draw(a.responseJSON, that.Index);
-						BioBLESS.change_stage(BioBLESS.device);
+                        PLUMB.device.draw(a.responseJSON, that.parent.Index);
+						PLUMB.change_stage(PLUMB.device);
 					}else
                         setTimeout(next, 50);
                 };
@@ -62,9 +107,9 @@ this.draw = function(devices){
         };
         that.onDragMove_e = function() {
             if(this.dragging) {
-                var newPosition = this.data.getLocalPosition(this.parent);
-                this.position.x = newPosition.x - 75;
-                this.position.y = newPosition.y - 35;
+                var newPosition = this.data.getLocalPosition(this.parent.parent);
+                this.parent.position.x = newPosition.x - 75;
+                this.parent.position.y = newPosition.y - 35;
             }
         };
 			
@@ -81,13 +126,13 @@ this.draw = function(devices){
             this.dragging = false;
             this.data = null;
 			if(!(this.position.x + 75 >= w - 220 && this.position.x + 75 <= w - 20 && this.position.y >= 110 && this.position.y <= h - 20)){
-				elements[elements.length] = that.DrawGate(devices[this.Index].icon);
+				elements[elements.length] = that.DrawGate(devices[this.Index]);
 				elements[elements.length - 1].position.x = this.childPosition.x - 75;
 				elements[elements.length - 1].position.y = this.childPosition.y - 35;
-				elements[elements.length - 1].interactive = true;
-                elements[elements.length - 1].buttonMode = true;
+				elements[elements.length - 1].graphics.interactive = true;
+                elements[elements.length - 1].graphics.buttonMode = true;
 				elements[elements.length - 1].Index = this.Index;
-				elements[elements.length - 1].on('mousedown', that.onDragStart_e)
+				elements[elements.length - 1].graphics.on('mousedown', that.onDragStart_e)
                     .on('touchstart', that.onDragStart_e)
                     .on('mouseup', that.onDragEnd_e)
                     .on('mouseupoutside', that.onDragEnd_e)
@@ -119,8 +164,8 @@ this.draw = function(devices){
 		that._logicGates = new Array();
 		that.logicGates = new Array();
 		for(var i = 0; i < devices.length; i++){
-		    that._logicGates[i] = this.DrawGate(devices[i].icon);
-			that.logicGates[i] = this.DrawGate(devices[i].icon);
+		    that._logicGates[i] = this.DrawGate(devices[i]);
+			that.logicGates[i] = this.DrawGate(devices[i]);
 			that._logicGates[i].position.x = w - 195;
 			that._logicGates[i].position.y = 140 + i * 100;
 			that.logicGates[i].position.x = w - 195;
@@ -186,7 +231,6 @@ this.draw = function(devices){
 		
 	    return this.stage;
 	};
-var a=$.getJSON("../misc/devices.json");
-setTimeout(function(){BioBLESS.home.draw(a.responseJSON)},100);
-return this;
+var a=$.getJSON("/misc/devices.json");
+setTimeout(function(){PLUMB.home.draw(a.responseJSON)},100);
 };
