74f4f0a15079c501d9d364ed8cc3df70d42ae535
ZSY <zsy9509@mail.ustc.edu.cn>
2015-08-12 17:20:17 +0800

    Simplified the backend code. =_=

--- backend/BioBLESS/urls.py
+++ backend/BioBLESS/urls.py
@@ -13,37 +13,19 @@ Including another URLconf
     1. Add an import:  from blog import urls as blog_urls
     2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
+import django
 from django.conf.urls import include, url
 from django.contrib import admin
 from BioBLESS.views.views import aplusb
-from BioBLESS.views.route import route_content, route_page, reg
-from BioBLESS.views.cache import cache, cache_dict
-from BioBLESS.views.pkgapi import api
-from test_samples import test_reaction_system_sample, test_parts_system_sample
+from BioBLESS.biosys.test_samples import test_reaction_system_sample, test_parts_system_sample
 
 
 urlpatterns = [
     url(r'^$', include('home.urls')),
     url(r'^aplusb/(\d+)/(\d+)/$', aplusb),
     url(r'^admin/', include(admin.site.urls)),
-    # Provide file/content
-    url(r'^route/(?P<pkgname>.*)/(?P<subpath>.*)', route_content),
-    # To show something HTML
-    url(r'^page/(?P<pkgname>.*)/(?P<subpath>.*)', route_page),
-    # Register
-    url(r'^register/(?P<pkgname>.*)', reg),
-    # Cache
-    url(r'^cache/(?P<pkgname>.*)/(?P<key>.*)', cache),
-    url(r'^cache_dict/(?P<pkgname>.*)/', cache_dict),
 
     # biosys
     url(r'^biosys/test-reaction-system-sample/$', test_reaction_system_sample),
-    url(r'^biosys/test-parts-system-sample/$', test_parts_system_sample),
-
-    url(r'^api/', api),
+    url(r'^biosys/test-parts-system-sample/$', test_parts_system_sample)
 ]
-
-
-
-
-
--- backend/BioBLESS/views/route.py
+++ backend/BioBLESS/views/route.py
@@ -5,114 +5,77 @@ This module provide universal interfaces for frontend and backend
 
 __author__ = 'zsy95_000'
 
-from uuid import uuid4
-import os
-from os import path
-from os.path import exists
-import importlib
-from BioBLESS.common import path_sbin
-from BioBLESS.controls import send
-from BioBLESS.models.settings import sbin, hostURL
-
-from django.http import HttpResponse
-from django.shortcuts import render_to_response
-from django.template import Template, Context
-
-
-def route_path_sbin(pkgname, subpath):
-    """
-    Return the path by package name.
-    WARNING: The path is with the format of local system
-    :param request: Request like /route/package_name/folder/.../filename
-    :return: path with package name
-    """
-    return path_sbin(os.path.join(pkgname, subpath))
+# from uuid import uuid4
+# import os
+# from os import path
+# from os.path import exists
+# import importlib
+# from BioBLESS.common import path_sbin
+# from BioBLESS.controls import send
+# from BioBLESS.models.settings import sbin, hostURL
+#
+# from django.http import HttpResponse
+# from django.shortcuts import render_to_response
+# from django.template import Template, Context
 
 
-def route_content(request, pkgname, subpath):
-    """
-    Return the package as download response.
-    If not found, you'll get a null file
-    :param request:
-    :param pkgname:
-    :param subpath:
-    :return:Download content
-    """
-
-    full_path = route_path_sbin(pkgname, subpath)
-    return send.warp(full_path) if exists(full_path) else HttpResponse("", content_type='application/octet-stream')
-
-
-def render_to_response_local(path, args):
-    """
-    Render local template file
-    :param path: File path
-    :param args: Parameters
-    :return: Rendered HttpResponse
-    """
-    with open(path) as f:
-        data = f.read()
-    return HttpResponse(Template(data).render(Context(args)))
-
-
-def route_page(request, pkgname, subpath, params={}):
-    """
-    Render a page with get/post parameters
-    If content_path is empty, set index.html as default
-    Make page unique with GUID, {{uuid}} targeted.
-    :param request: Request like r'^page/(?P<pkgname>.*)/(?P<subpath>.*)'
-    :param content_path: The content under the
-    :param params: The dictionary contains route data.
-    :return: The page
-    """
-    full_path = route_path_sbin(pkgname, subpath)
-    params.update(request.GET.__dict__)
-    params.update(request.POST)
-    params['uuid'] = uuid4()
-    params['host'] = hostURL
-    if subpath == "":
-        full_path = path.join(full_path, "index.html")
-        return render_to_response_local(full_path, params) \
-            if exists(full_path) \
-            else HttpResponse("This package do not have a main page")
-    else:
-        return render_to_response_local(full_path, params) \
-            if exists(full_path) \
-            else HttpResponse("Page not found: " + full_path)
-
-
-def reg(request, pkgname):
-    """
-    Load the specified package
-    * means all.
-    :param request:
-    :param pkgname: Name
-    :return:
-    """
-    if pkgname == "*":
-        register_all()
-    else:
-        register(pkgname)
-    return HttpResponse("Done.")
-
-
-def register_all():
-    """
-    Start init all packages.
-    NOTE : Cannot be used in __init__
-    :param pkgname: package name
-    :return: None
-    """
-    map(register, [i for i in os.listdir(sbin) if path.isdir(path.join(sbin, i))])
-
-
-def register(pkgname):
-    """
-    Start init a package.
-    NOTE : Cannot be used in __init__
-    :param pkgname: package name
-    :return: None
-    """
-    fn = path.join(sbin, pkgname, "__init__.py")
-    if exists(fn):
-        __import__(pkgname)
+# def route_path_sbin(pkgname, subpath):
+#     """
+#     Return the path by package name.
+#     WARNING: The path is with the format of local system
+#     :param request: Request like /route/package_name/folder/.../filename
+#     :return: path with package name
+#     """
+#     return path_sbin(os.path.join(pkgname, subpath))
+#
+#
+# def route_content(request, pkgname, subpath):
+#     """
+#     Return the package as download response.
+#     If not found, you'll get a null file
+#     :param request:
+#     :param pkgname:
+#     :param subpath:
+#     :return:Download content
+#     """
+#
+#     full_path = route_path_sbin(pkgname, subpath)
+#     return send.warp(full_path) if exists(full_path) else HttpResponse("", content_type='application/octet-stream')
+#
+#
+# def render_to_response_local(path, args):
+#     """
+#     Render local template file
+#     :param path: File path
+#     :param args: Parameters
+#     :return: Rendered HttpResponse
+#     """
+#     with open(path) as f:
+#         data = f.read()
+#     return HttpResponse(Template(data).render(Context(args)))
+#
+#
+# def route_page(request, pkgname, subpath, params={}):
+#     """
+#     Render a page with get/post parameters
+#     If content_path is empty, set index.html as default
+#     Make page unique with GUID, {{uuid}} targeted.
+#     :param request: Request like r'^page/(?P<pkgname>.*)/(?P<subpath>.*)'
+#     :param content_path: The content under the
+#     :param params: The dictionary contains route data.
+#     :return: The page
+#     """
+#     full_path = route_path_sbin(pkgname, subpath)
+#     params.update(request.GET.__dict__)
+#     params.update(request.POST)
+#     params['uuid'] = uuid4()
+#     params['host'] = hostURL
+#     if subpath == "":
+#         full_path = path.join(full_path, "index.html")
+#         return render_to_response_local(full_path, params) \
+#             if exists(full_path) \
+#             else HttpResponse("This package do not have a main page")
+#     else:
+#         return render_to_response_local(full_path, params) \
+#             if exists(full_path) \
+#             else HttpResponse("Page not found: " + full_path)
