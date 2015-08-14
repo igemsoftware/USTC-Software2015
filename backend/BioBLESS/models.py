9a888775ca5cb1046ecb000ac6ad0a7f76d689ed
ctyi <1997cui@sina.com>
2015-08-14 10:18:35 +0800

    small bug fix

--- backend/BioBLESS/models.py
+++ backend/BioBLESS/models.py
@@ -1,12 +1,18 @@
-__author__ = 'ctyi'
 """
 This is the model for the parts table
 """
+__auther__ = 'ctyi'
 from django.db import models
 class parts(models.Model):
+    """
+    This is the parts database
+    """
     name = models.CharField(max_length=15)
     type = models.CharField(max_length=25)
     description = models.CharField(max_length=150)
     sequence = models.CharField(max_length=68000)
     uid = models.AutoField(primary_key=True)
     id = models.IntegerField(unique=True)
+
+    def __unicode__(self):
+        return self.name
--- backend/BioBLESS/views/parts.py
+++ backend/BioBLESS/views/parts.py
@@ -12,6 +12,12 @@ from rest_framework.renderers import JSONRenderer
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+
+class PartsSerializer(serializers.Serializer):
+    # id = serializers.IntegerField()
+    description = serializers.CharField()
+    # sequence = serializers.CharField()
+    # type = serializers.CharField()
 class PartsView(APIView):
     """
     This function return frontend parts info.
@@ -22,14 +28,17 @@ class PartsView(APIView):
         """
         deal with the POST method only
         """
-        pre_filter={}
-        if "id" in request.data:
-            pre_filter['id'] = request.data['id']
-        for key in request.data:
-            if key != "id":
-                pre_filter[key+"__contains"]=request.data[key]
-        queryset=parts.objects.filter(**pre_filter)
-        ResopnseDict={}
-        ResopnseDict["data"]=queryset
-        ResopnseDict["status"]="SUCCESS"
-        return Response(ResopnseDict)
+        response_total = []
+        for item in request.data:
+            pre_filter = {}
+            if "id" in item:
+                pre_filter['id'] = item['id']
+            for key in item:
+                if key != "id":
+                    pre_filter[key + "__contains"] = item[key]
+            queryset = parts.objects.filter(**pre_filter)
+            response_total.extend(queryset.values())
+        response_dict = {}
+        response_dict['status'] = "SUCCESS"
+        response_dict['data'] = response_total
+        return Response(response_dict)
