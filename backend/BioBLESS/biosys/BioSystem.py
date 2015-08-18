9cb69ad288b6dfc7745ccf811a2bd5907b24c73a
E-Neo <e-neo@qq.com>
2015-08-18 14:42:29 +0800

    add biosystem

---
+++ backend/BioBLESS/biosys/BioSystem.py
@@ -0,0 +1,18 @@
+class BioSystem(object):
+    # lizhi = simplejson.loads()
+    def __init__(self, spec, lizhi):
+        d_lizhi = self.__lizhi_to_dict(lizhi)
+        devices = []
+        l_nodes = [i for i in spec['nodes']]
+        for i in range(len(l_nodes)):
+            l_input = [j for j in spec['arcs'] if j['to'] == i]
+            if l_node[i] != 'INPUT':
+                devices.append(DeviceSystem(lizhi[l_nodes[i]], spec).as_reaction_system(str(i), l_input, str(i)))
+
+    def __lizhi_to_dict(self, lizhi):
+        d_lizhi = {}
+        for i in lizhi:
+            d_lizhi[i['id']] = i
+        return d_lizhi
+
+    def simulate(self, record=[0, current]):
