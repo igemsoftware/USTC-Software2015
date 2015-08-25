9e42d66cfa4c5519acc3d0d0658d76c96160ccfe
Lingyuan Ji <LingyuanJi@Proton.local>
2015-08-25 13:36:36 +0800

    optimize variables name

--- backend/BioBLESS/biosys/reaction_system_sample.py
+++ backend/BioBLESS/biosys/reaction_system_sample.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-from ReactionSystem import ReactionSystem
+from reaction_system import ReactionSystem
 A=ReactionSystem([[["a"],["b"],1]],[["a",100],"b"])
 B=ReactionSystem([[["b"],["c"],1]],[["b",100],"c"])
 C=A+B
