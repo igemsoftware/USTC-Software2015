040c042017b629a1294d190b835feaa30b86d82f
Yishharu <lizhi555@mail.ustc.edu.cn>
2015-08-25 01:35:27 -0700

    a relative good regulatory para

--- backend/BioBLESS/biosys/test_data.py
+++ backend/BioBLESS/biosys/test_data.py
@@ -10,13 +10,13 @@ system_data = {
     "simulation_parameters": [
     {
         "e1": {
-            "trans1": 0.001,#0.01,
+            "trans1": 0.00001,#0.01,
             "trans2": 0.5,
             "decay1": 0.2,
             "decay2": 0.1
         },
         "e2": {
-            "reg": 40
+            "reg": 3
         },
         "e3": {
             "trans1": 0.01,#0.01,
@@ -25,10 +25,10 @@ system_data = {
             "decay2": 0.1
         },
         "e4": {
-            "reg": 20
+            "reg": 3
         },
         "e5": {
-            "reg": 20
+            "reg": 3
         },
         "e6": {
             "trans1": 0.01,
@@ -45,7 +45,7 @@ system_data = {
     {
         "device_parameter": {
             "initial":[
-                0
+                500
             ]
         }
     }
