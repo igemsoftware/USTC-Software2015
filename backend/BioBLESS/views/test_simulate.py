176c2edd83703b225c2944589334e887e2083121
ctyi <1997cui@sina.com>
2015-08-28 22:52:12 +0800

    Change test for cache

--- backend/BioBLESS/views/test_simulate.py
+++ backend/BioBLESS/views/test_simulate.py
@@ -7,6 +7,7 @@ from django.test import TestCase
 import simplejson
 import shutil
 import os
+import time
 
 import BioBLESS.biosys.data_test as input_dict
 
@@ -24,12 +25,19 @@ class TestSimulate(TestCase):
         assert isinstance(response_dict, list)
 
     def test_with_cache(self):
+        cold_time_start = time.time()
         response = self.client.post('/simulate/', simplejson.dumps(input_dict.system_data),
                                     content_type="application/json")
+        cold_time_end = time.time()
+        cold_time = cold_time_end - cold_time_start
         self.assertEqual(response.status_code, 200)
         response_dict = simplejson.loads(response.content)
+        hot_time_start = time.time()
         response = self.client.post('/simulate/', simplejson.dumps(input_dict.system_data),
                                     content_type="application/json")
+        hot_time_end = time.time()
+        hot_time = hot_time_end - hot_time_start
+        self.assertGreater(cold_time, hot_time)
         self.assertEqual(response.status_code, 200)
         response_dict_cache = simplejson.loads(response.content)
         self.assertEqual(response_dict, response_dict_cache)
