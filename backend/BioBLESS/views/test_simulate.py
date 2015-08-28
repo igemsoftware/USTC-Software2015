2bcd614bd721df427673f8fdfc4bfaab06728e96
ctyi <1997cui@sina.com>
2015-08-28 22:13:11 +0800

    Change test for cache

--- backend/BioBLESS/views/test_simulate.py
+++ backend/BioBLESS/views/test_simulate.py
@@ -5,6 +5,8 @@ This is the Unit test for parts API.
 __author__ = 'ctyi'
 from django.test import TestCase
 import simplejson
+import shutil
+import os
 
 import BioBLESS.biosys.data_test as input_dict
 
@@ -12,10 +14,31 @@ import BioBLESS.biosys.data_test as input_dict
 class TestSimulate(TestCase):
     """A test for /parts/ API.
     """
-    def test_simple_dict(self):
+
+    def test_without_cache(self):
         response = self.client.post('/simulate/', simplejson.dumps(input_dict.system_data),
                                     content_type="application/json")
         self.assertEqual(response.status_code, 200)
         response_dict = simplejson.loads(response.content)
         # print response_dict
         assert isinstance(response_dict, list)
+
+    def test_with_cache(self):
+        response = self.client.post('/simulate/', simplejson.dumps(input_dict.system_data),
+                                    content_type="application/json")
+        self.assertEqual(response.status_code, 200)
+        response_dict = simplejson.loads(response.content)
+        response = self.client.post('/simulate/', simplejson.dumps(input_dict.system_data),
+                                    content_type="application/json")
+        self.assertEqual(response.status_code, 200)
+        response_dict = simplejson.loads(response.content)
+        # print response_dict
+        assert isinstance(response_dict, list)
+
+    def setUp(self):
+        shutil.rmtree('../cache')
+        os.mkdir('../cache')
+
+    def tearDown(self):
+        shutil.rmtree('../cache')
+        os.mkdir('../cache')
