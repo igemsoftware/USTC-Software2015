f71bf009adfa2a97aec4c42d690c765219ebe469
suquark <zsy9509@mail.ustc.edu.cn>
2015-08-30 21:12:43 +0800

    Beautify the code.

--- backend/BioBLESS/biosys/test_reaction.py
+++ backend/BioBLESS/biosys/test_reaction.py
@@ -6,18 +6,17 @@ except ImportError:
 
 from reaction_system import ReactionSystem
 
-
 if __name__ == "__main__":
-    A=ReactionSystem([[["a"],["b"],1]],[["a",100],"b"])
-    B=ReactionSystem([[["b"],["c"],1]],[["b",100],"c"])
-    C=A+B
+    A = ReactionSystem([[["a"], ["b"], 1]], [["a", 100], "b"])
+    B = ReactionSystem([[["b"], ["c"], 1]], [["b", 100], "c"])
+    C = A + B
     print C.species
-    C.show_simulate([],10,["a","b","c"])
+    C.show_simulate([], 10, ["a", "b", "c"])
 else:
     class TestReaction(TestCase):
         def test_reaction(self):
-            A=ReactionSystem([[["a"],["b"],1]],[["a",100],"b"])
-            B=ReactionSystem([[["b"],["c"],1]],[["b",100],"c"])
-            C=A+B
+            A = ReactionSystem([[["a"], ["b"], 1]], [["a", 100], "b"])
+            B = ReactionSystem([[["b"], ["c"], 1]], [["b", 100], "c"])
+            C = A + B
             print C.species
             C.simulate([], 10)
