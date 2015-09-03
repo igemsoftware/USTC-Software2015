78f12c6da992be01a372f8560da6246d9cb70079
zh19970205 <zh19970205@126.com>
2015-09-03 21:07:36 +0800

    test

--- backend/BioBLESS/biosys/data_test.py
+++ backend/BioBLESS/biosys/data_test.py
@@ -10,7 +10,7 @@ system_data = {
     "simulation_parameters": [
         {
             "e1": {
-                "trans1": 0.01, 
+                "trans1": 0.0001, 
                 "trans2": 0.5,
                 "decay1": 0.2,
                 "decay2": 0.1
@@ -45,7 +45,7 @@ system_data = {
         {
             "device_parameter": {
                 "initial": [
-                    50
+                    0
                 ]
             }
         }
