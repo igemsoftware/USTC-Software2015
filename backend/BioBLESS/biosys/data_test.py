16e2b0030c2251dc396fee83ae8c374563b49d84
zh19970205 <zh19970205@126.com>
2015-09-03 21:11:19 +0800

    test

--- backend/BioBLESS/biosys/data_test.py
+++ backend/BioBLESS/biosys/data_test.py
@@ -51,3 +51,58 @@ system_data = {
         }
     ]
 }
+
+system_data_mixed = {		
+    "nodes": ["INPUT", "NOT2"],		
+    "system_parameter":		
+        {		
+            "time": 1000		
+        },		
+    "arcs": [		
+        {"from": 0, "to": 1}		
+    ],		
+    "simulation_parameters": [		
+        {		
+		
+            "device_parameter": {		
+                "initial": [		
+                    50		
+                ]		
+            }		
+        },		
+        {		
+            "e2": {		
+                "reg": 3		
+            },		
+            "e1": {		
+                "trans1": 0.00001,  # 0.01,		
+                "trans2": 0.5,		
+                "decay1": 0.2,		
+                "decay2": 0.1		
+            },		
+            "e4": {		
+                "reg": 3		
+            },		
+            "e3": {		
+                "trans1": 0.01,  # 0.01,		
+                "trans2": 0.5,		
+                "decay1": 0.2,		
+                "decay2": 0.1		
+            },		
+            "e5": {		
+                "reg": 3		
+            },		
+            "e6": {		
+                "trans1": 0.01,		
+                "trans2": 0.5,		
+                "decay1": 0.2,		
+                "decay2": 0.1		
+            },		
+            "device_parameter": {		
+                "initial": [		
+                    40, 40, 40		
+                ]		
+            }		
+        }		
+    ]		
+}
