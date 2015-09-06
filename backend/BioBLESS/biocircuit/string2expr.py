68c6b9273a6042ff69790375656a1c30c20e6e67
E-Neo <e-neo@qq.com>
2015-09-07 00:18:58 +0800

    espresso complete but not connected

---
+++ backend/BioBLESS/biocircuit/string2expr.py
@@ -0,0 +1,44 @@
+from espresso import espresso
+from math import log
+
+def string2expr(string):
+    """Compute minimal two-level sum-of-products form.
+
+    Parameters
+    ----------
+    string : string
+        A string of zeros and ones representing a truthtable.
+
+    Returns
+    -------
+    expr : list
+        Minimal two-level SOP form.
+        Examples
+    --------
+    A truthtable:
+    |---+---+---+-----|
+    | A | B | C | out |
+    |---+---+---+-----|
+    | 0 | 0 | 0 |  1  |
+    | 0 | 0 | 1 |  0  |
+    | 0 | 1 | 0 |  1  |
+    | 0 | 1 | 1 |  1  |
+    | 1 | 0 | 0 |  0  |
+    | 1 | 0 | 1 |  1  |
+    | 1 | 1 | 0 |  0  |
+    | 1 | 1 | 1 |  -  |
+    |---+---+---+-----|
+    string = '1011010-'
+    """
+    ninput = int(log(len(string), 2))
+    cover = []
+    for i in range(len(string)):
+        row = bin(i).replace('0b', '')
+        row = '0' * (ninput - len(row)) + row
+        tmp = [int(k) + 1 for k in row]
+        cover.append((tuple(tmp), (int(string[i]),)))
+    cover = set(cover)
+    result = list(espresso(ninput, 1, cover))
+    result = [i[0] for i in result]
+    result = [''.join(['X' if i == 3 else str(i-1) for i in j]) for j in result]
+    return result
