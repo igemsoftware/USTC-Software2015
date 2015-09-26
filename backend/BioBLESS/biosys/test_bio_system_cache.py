cadf86a947bb36eabd75f655674b35f58736d430
zh19970205 <zh19970205@126.com>
2015-09-26 10:23:25 +0800

    covery

--- backend/BioBLESS/biosys/test_bio_system_cache.py
+++ backend/BioBLESS/biosys/test_bio_system_cache.py
@@ -10,23 +10,23 @@ if __name__ == "__main__":
     # This can be used as unittest, for if it failed on a certain python version, you cannot use the module safely.
     # --------------------------------------------------------
     # This shows True, for python will check the dict recurrently and ignore the order.
-    dict1 = {"0": (3.1416, "0:T~1", {"0:T~2I0": 1, "b": 3.14}), "1": "c1"}
-    dict2 = {"1": "c1", "0": (3.1416, "0:T~1", {"b": 3.14, "0:T~2I0": 1})}
-    print dict1 == dict2
+    #dict1 = {"0": (3.1416, "0:T~1", {"0:T~2I0": 1, "b": 3.14}), "1": "c1"}
+    #dict2 = {"1": "c1", "0": (3.1416, "0:T~1", {"b": 3.14, "0:T~2I0": 1})}
+    #print dict1 == dict2
     # This shows False, for the order of items in the list are very important
-    list1 = [1, 3]
-    list2 = [3, 1]
-    print list1 == list2
+    #list1 = [1, 3]
+    #list2 = [3, 1]
+    #print list1 == list2
     # Set, ignore the order but may change the count. (True)
-    print set(list1) == set(list2)
+    #print set(list1) == set(list2)
     # There never seems to be a hash for a dict (None). Nasty!
-    print dict1.__hash__
+    #print dict1.__hash__
     # Just dump it in order. Important.
-    print dump_ord(biosystem_sample1)
+    #print dump_ord(biosystem_sample1)
     # Emm... So I only have to json-hash it, right?
-    print hash_gates_and_simulation(biosystem_sample1)
+    #print hash_gates_and_simulation(biosystem_sample1)
 
-    print compare_biosystem(biosystem_sample1, biosystem_sample2)
+    #print compare_biosystem(biosystem_sample1, biosystem_sample2)
 else:
     class TestReaction(TestCase):
         def test(self):
