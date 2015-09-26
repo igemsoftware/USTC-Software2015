2fe48e7d5bbf1c0acd1e590f5f375f3e55dad617
zh19970205 <zh19970205@126.com>
2015-09-26 10:35:13 +0800

    mkdir temp

--- backend/BioBLESS/biosys/bio_system.py
+++ backend/BioBLESS/biosys/bio_system.py
@@ -1,15 +1,15 @@
 import os, json, random
 
 def simulate(dict_data):
-    #os.system("mkdir temp")
+    os.system("mkdir temp 1>/dev/null 2>/dev/null")
     temp1 = "./temp/"+str(random.random())
     temp2 = "./temp/"+str(random.random())
     to_write = json.dumps(dict_data)
     in_file = open(temp1, 'w')
     in_file.write(to_write)
     in_file.close()
-    os.system('pypy bio_system_old.py<'+temp1+' >'+temp2)
-    os.system('pypy BioBLESS/biosys/bio_system_old.py<'+temp1+' >'+temp2)
+    if os.system('pypy bio_system_old.py<'+temp1+' >'+temp2) != 0:
+        os.system('pypy BioBLESS/biosys/bio_system_old.py<'+temp1+' >'+temp2)
     out_file = open(temp2, "r")
     res = out_file.read()
     out_file.close()
